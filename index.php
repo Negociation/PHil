7c7a2237955aee80bbb0f6fe09d1090c0f73c2f4
Negociation <32465944+Negociation@users.noreply.github.com>
2021-01-21 09:41:24 -0300

    Adicionado o Index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+//UTF-8(Portuguese and Spanish Accents for PHP)
+header("Content-Type: text/html; charset=utf-8",true);	
+
+// + Call the Configuration File Once
+require_once('./app/core.php');
+?>
